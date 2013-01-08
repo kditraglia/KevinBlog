c8954d64510ff64d67642e8eddac806913eedf86
Kevin DiTraglia <ktd2@case.edu>
2013-01-07 19:50:45 -0500

    Created Blog!

--- KevinBlog/KevinBlog/urls.py
+++ KevinBlog/KevinBlog/urls.py
@@ -5,13 +5,13 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'KevinBlog.views.home', name='home'),
-    # url(r'^KevinBlog/', include('KevinBlog.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    (r'^$', 'djangorocks.blog.views.index'),
+    url(
+        r'^blog/view/(?P<slug>[^\.]+).html',
+        'djangorocks.blog.views.view_post',
+        name='view_blog_post'),
+    url(
+        r'^blog/category/(?P<slug>[^\.]+).html',
+        'djangorocks.blog.views.view_category',
+        name='view_blog_category'),
 )
