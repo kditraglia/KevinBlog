076301eeca3d441839379f80a351a12d162c7dec
Kevin DiTraglia <ktd2@case.edu>
2013-01-07 20:09:16 -0500

    changed incorrect views reference

--- KevinBlog/blog/views.py
+++ KevinBlog/blog/views.py
@@ -1,5 +1,5 @@
 
-from djangorocks.blog.models import Blog, Category
+from KevinBlog.blog.models import Blog, Category
 from django.shortcuts import render_to_response, get_object_or_404
 
 def index(request):
