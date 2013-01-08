a2c6fb26174b644df11783df4158dbaa12851289
Kevin DiTraglia <ktd2@case.edu>
2013-01-07 20:16:52 -0500

    again

--- KevinBlog/KevinBlog/urls.py
+++ KevinBlog/KevinBlog/urls.py
@@ -5,13 +5,13 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^$', 'KevinBlog.blog.views.index'),
+    (r'^$', 'blog.views.index'),
     url(
         r'^blog/view/(?P<slug>[^\.]+).html',
-        'KevinBlog.blog.views.view_post',
+        'blog.views.view_post',
         name='view_blog_post'),
     url(
         r'^blog/category/(?P<slug>[^\.]+).html',
-        'KevinBlog.blog.views.view_category',
+        'blog.views.view_category',
         name='view_blog_category'),
 )
