74141ff5af186420951ca5f513bc3cc5517f067c
Kevin <kevin@kevin-virtual-machine.(none)>
2013-01-10 22:02:02 -0500

    commiting test

--- KevinBlog/KevinBlog/settings.py
+++ KevinBlog/KevinBlog/settings.py
@@ -58,14 +58,10 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-<<<<<<< HEAD
-
-=======
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(__file__), '..','blog/static')
->>>>>>> dbdfc7a52c92df0545f804ec9630b7dd21af996a
+    #os.path.join(os.path.dirname(__file__), '..','blog/static')
 )
 
 # List of finder classes that know how to find static files in
